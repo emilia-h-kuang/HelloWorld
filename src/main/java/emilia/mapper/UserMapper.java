1287f6664eb50a7e9771a2115256a24f942d4439
Emilia H K <emilia.h.k@outlook.com>
2022-10-27 21:31:16 +0800

    day01 asdfasdfasdf asdfasdfasdf asdfasd
    修复了一个bug
    添加一个功能
    修复了day01-product的一个空指针问题

--- src/main/java/emilia/mapper/UserMapper.java
+++ src/main/java/emilia/mapper/UserMapper.java
@@ -6,6 +6,9 @@ import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 public interface UserMapper {
+
+
+
     @Select("select * from tb_user where username =#{username} and password=#{password}")
     User selectUser(@Param("username") String username, @Param("password") String password);
 
